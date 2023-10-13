a300e681a601f45f9041d27ba78cba10a73d4e1c
“Nathan <schroedernathan2011@icloud.com>
2023-10-12 22:18:07 -0400

    Initial Commit

---
+++ SongOfTheDay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SongOfTheDay
+//
+//  Created by Nathan Schroeder on 2023-10-12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
