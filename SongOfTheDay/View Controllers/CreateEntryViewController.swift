eaca7c5e5844fcdb25e6f07a323d3e8eecc1c210
“Nathan <schroedernathan2011@icloud.com>
2023-10-12 22:28:02 -0400

    Set up tab bar

---
+++ SongOfTheDay/View Controllers/CreateEntryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CreateEntryViewController.swift
+//  SongOfTheDay
+//
+//  Created by Nathan Schroeder on 2023-10-12.
+//
+
+import UIKit
+
+class CreateEntryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
