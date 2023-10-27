6789b1ce86ca38f27ea6b16f0ecfb6f31586e9e6
“Nathan <schroedernathan2011@icloud.com>
2023-10-27 01:18:37 -0400

    Created custom cell layout for the journal entries

---
+++ SongOfTheDay/JournalEntryCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  JournalEntryCollectionViewCell.swift
+//  SongOfTheDay
+//
+//  Created by Nathan Schroeder on 2023-10-27.
+//
+
+import UIKit
+
+class JournalEntryCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var songArtwork: UIView!
+    @IBOutlet var songInfo: UILabel!
+    @IBOutlet var goodDayButton: UIImageView!
+    @IBOutlet var badDayButton: UIImageView!
+    @IBOutlet var journalDate: UILabel!
+}
